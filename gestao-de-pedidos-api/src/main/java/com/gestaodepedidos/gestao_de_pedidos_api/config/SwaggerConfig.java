4c05e41479ab25c26a82f65c1d9d918e3cc586fd
demersonreis <demersonsilvadosreis@gmail.com>
2024-10-24 19:02:40 -0300

    swagger

---
+++ gestao-de-pedidos-api/src/main/java/com/gestaodepedidos/gestao_de_pedidos_api/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.gestaodepedidos.gestao_de_pedidos_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+@Configuration
+public class SwaggerConfig {
+
+
+	@Bean
+	public OpenAPI springShopOpenAPI() {
+	    return new OpenAPI()
+	            .info(new Info().title("Gestão Pedidos")
+	                    .version("v0.0.0")
+	                    .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+	            .externalDocs(new ExternalDocumentation()
+	                    .description("Documento contendo todas as regras de negócio.")
+	                    .url("https://springshop.wiki.github.org/docs"))
+	            .components(new Components()
+	                    .addSecuritySchemes("bearer-key", new SecurityScheme()
+	                            .type(SecurityScheme.Type.HTTP)
+	                            .scheme("bearer")
+	                            .bearerFormat("JWT")))
+	            .addSecurityItem(new SecurityRequirement().addList("bearer-key"));
+	}
+}
+
