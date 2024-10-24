73943a5671a93e604fa1a3a678baa54c3a2f9231
demersonreis <demersonsilvadosreis@gmail.com>
2024-10-24 19:28:58 -0300

    swagger

--- gestao-de-pedidos-api/src/main/java/com/gestaodepedidos/gestao_de_pedidos_api/controller/CustomersController.java
+++ gestao-de-pedidos-api/src/main/java/com/gestaodepedidos/gestao_de_pedidos_api/controller/CustomersController.java
@@ -3,6 +3,7 @@ package com.gestaodepedidos.gestao_de_pedidos_api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,18 @@ import com.gestaodepedidos.gestao_de_pedidos_api.dto.CustomersDTO;
 import com.gestaodepedidos.gestao_de_pedidos_api.dto.MessageDTO;
 import com.gestaodepedidos.gestao_de_pedidos_api.service.CustomersService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Customers", description = "APIs relacionadas a clientes")
 @RequestMapping(value = "/customers")
 public class CustomersController {
 
+
 	@Autowired
 	private CustomersService customersService;
-
+    @Operation(summary = "Registrar um novo cliente", description = "Cria um novo cliente e salva no banco de dados.")
 	@PostMapping("/registration")
 	public ResponseEntity<MessageDTO> customerRegistration(@RequestBody CustomersDTO dto) {
 		MessageDTO response = customersService.customerRegistration(dto);
@@ -32,5 +38,10 @@ public class CustomersController {
 		}
 
 	}
-
+	
+	@GetMapping("/view")
+	public ResponseEntity<?> customerView(){
+		return null;
+		}
+ 
 }
